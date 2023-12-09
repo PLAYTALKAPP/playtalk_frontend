7fe2902125a0978b1e7edb3bd2a748e603083bcf
mlulucky <dmswjdans@gmail.com>
2023-12-09 11:00:38 +0900

    feat: next 설정파일

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+	compiler: {
+		styledComponents: true, // 스타일컴포넌트 설정 (서버사이드에서 스타일 적용하기 위해서는 pages/_document.tsx 커스텀 문서 작성)
+	},
+	// async rewrites() { 
+	// 	return [
+	// 		{
+	// 			source: "/:path*",
+	// 			destination: "http://localhost:8080/:path*"
+	// 		},
+	// 	]
+	// },
 }
 
 module.exports = nextConfig
