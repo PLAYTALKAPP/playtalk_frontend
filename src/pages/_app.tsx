7a2064cac851cc2b671fc60d59fa811fc2d8796c
mlulucky <dmswjdans@gmail.com>
2023-12-09 10:52:21 +0900

    feat: app 컴포넌트에 헤더nav 컴포넌트 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import './styles/globals.css'
+import "tailwindcss/tailwind.css"; // 이것만 추가해주면됨 (create next app 으로 tailwind.css 적용한 경우)
+import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import HeaderNav from "@/components/template/HeaderNavBar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+		<>
+			<HeaderNav/>
+			<Component {...pageProps} />
+		</>
+	)
 }
