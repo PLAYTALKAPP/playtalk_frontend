5b994428bf692e7178218c6eea85c5f0adefd4ae
mlulucky <dmswjdans@gmail.com>
2023-12-09 10:50:29 +0900

    feat: 헤더nav 컴포넌트 생성

---
+++ src/components/template/HeaderNavBar.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/organism/Header";
+import Nav from "@/components/organism/Nav";
+
+const HeaderNavBar = () => {
+	return(
+		<div>
+			<Header/>
+			<Nav/>
+		</div>
+	)
+}
+
+export default HeaderNavBar;
